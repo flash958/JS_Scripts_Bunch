132c0012874a1b8f0282c618ed08512ef3c99298
Shivam Saxena <saxena958@gmail.com>
2025-05-29 17:41:23 +0530

    Adding datatypes

---
+++ Basics1/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello")
+// alert(5+5) // We're using node.js not the browser
+
+console.log(3+3)
+
+console.log("Development")
+
+let flag = true //boolean 
+let name = "Shivam" //string
+let digit = 50 //number
+let blank //undefined
+let invalid = null //object
+
+console.table([flag,name,digit,blank,invalid])
+
+console.table([typeof(flag),typeof(name),typeof(digit),typeof(blank),typeof(invalid)])
+
+
