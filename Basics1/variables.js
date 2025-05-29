cb866ed763d4a859107d0ed6fe8f780435eb5ba8
Shivam Saxena <saxena958@gmail.com>
2025-05-29 17:16:27 +0530

    Adding variable file

---
+++ Basics1/variables.js
@@ -0,0 +1,36 @@
+const a=10
+console.log(a)
+
+// Variables declaration
+
+const account_id = 7412168 // This value can't be changed
+let account_email = "saxena958@gmail.com" 
+var accout_pass = "1212"
+account_city = "Agra" // we can declare variable in this way but it's non standarized
+
+let account_state
+
+// Modification of variables
+// account_id = 10  // Not allowed
+
+/* 
+We can use both ways to declare any variable using let, var
+But here noadays we generally use let
+*/
+
+
+account_email = 'newemail.com'
+accout_pass = "9999"
+account_city = "Metro"
+console.log(account_id)
+
+console.table([account_id,account_email,accout_pass,account_city, account_state])
+
+
+
+
+
+
+
+
+
