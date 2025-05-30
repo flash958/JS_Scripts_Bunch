f499821d6652884ef733196ee6726be78db2076c
Shivam Saxena <saxena958@gmail.com>
2025-05-30 13:57:25 +0530

    Added Conversion

---
+++ Basics1/conversion_operations.js
@@ -0,0 +1,31 @@
+let score = "33abc"
+// console.log(typeof score)
+// console.log(typeof(score))
+
+score = 'Shiv'
+
+let valueInNumber = Number(score) //It'll comvert the number irrespective of string part with it
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// "33" -> 33
+// "33abc" -> Nan
+// true -> 1
+// false -> 0
+
+let isloggedin = ""
+let  booleanisloggedin = Boolean(isloggedin)
+// console.log(booleanisloggedin)
+// console.log(typeof booleanisloggedin)
+
+// 1 -> true
+// 0 -> false
+// "hitesh" -> true
+// "" -> false
+
+
+let demo_num = 44
+let strnum = String(demo_num)
+console.log(strnum)
+console.log(typeof strnum)
