c36c51c43577b687e201f975a667fdd8f0b1c94e
Shivam Saxena <saxena958@gmail.com>
2025-05-31 11:50:35 +0530

    Updated conversion file

--- Basics1/conversion_operations.js
+++ Basics1/conversion_operations.js
@@ -27,5 +27,39 @@ let  booleanisloggedin = Boolean(isloggedin)
 
 let demo_num = 44
 let strnum = String(demo_num)
-console.log(strnum)
-console.log(typeof strnum)
+// console.log(strnum)
+// console.log(typeof strnum)
+
+
+// *************** Operations *****************************
+
+let value = 3
+let neg_val = -value
+// console.log(neg_val)
+
+let str1 = "hitesh"
+let str2 = " hitesh"
+let str3 = str1 + str2
+// console.log(str3)
+// console.log("1" + 2 + 2)
+// console.log(1 + "2")
+// console.log(2 + 2 + "1")
+
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2, num3
+num1=num2=num3=2+2
+// console.table([num1,num2,num3])
+
+console.log(+true)
+
+let game_counter = 100
+const a=++game_counter
+console.log(a)
+const b=game_counter++
+console.log(b)
+
+
+
+
